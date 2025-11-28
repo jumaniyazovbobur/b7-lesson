262dc228c1f953c6e19cb897117f76e5fcb00874
= <jumaniyazov.bobur@mail.ru>
2025-11-28 07:35:54 -0800

    Book detail added

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,4 @@
 public class Book {
+    private String title;
+    private String author;
 }
--- src/Subject.java
+++ src/Subject.java
@@ -1,2 +1,8 @@
 public class Subject {
+
+    // git remote add origin https://github.com/jumaniyazovbobur/b7-lesson.git
+
+
+    //  git push -u  origin  master
+
 }
